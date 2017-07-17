ee62b63c7753d50075970d3bf50cbe06d0b36ac8
caozheng <caozheng@xinlonghang.cn>
2017-07-17 18:42:31 +0800

    up

--- app/doctor.php
+++ app/doctor.php
@@ -10,15 +10,15 @@
 <div data-role="page">
   <div data-role="header">
   <div data-role="header"><a href="#" data-role="button" data-icon="arrow-l" data-rel="back">后退</a>
-  <h1>技工录入</h1>
+  <h1>医生录入</h1>
   </div>
 
   <div data-role="content" data-theme="c">
     <form method="post" action="demoform.asp">
       <div data-role="fieldcontain">
-      	<div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">医疗机构：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="医疗机构"></div>
-        <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">医疗专家：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="医疗专家"></div>
-        <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">患者姓名：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="患者姓名"></div>
+      	<div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">医疗机构：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="技工录入自动获取"></div>
+        <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">医疗专家：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="技工录入自动获取"></div>
+        <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">患者姓名：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="技工录入自动获取"></div>
         <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">患者性别：</div><div style="text-align: center; width: 75%; float: left;">
         	<fieldset data-role="controlgroup">
         			<label for="male">男性</label>
@@ -27,7 +27,7 @@
         		    <input type="radio" name="gender" id="female" value="female">	
             </fieldset>
         </div>
-        <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">患者年龄：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="患者年龄"></div> 
+        <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">患者年龄：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="技工录入自动获取"></div> 
         <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">选择牙位：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder="左上|右上|左下|右下"></div>
         <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">制作单位：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder=""></div>
         <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">录入时间：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="text" name="fullname" id="fullname" placeholder=""></div>
@@ -40,7 +40,8 @@
 		        </select>
 	      </fieldset>
         </div>
-        <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">修复体图片：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="file" name="file" id="file" value=""></div>
+        <div style="height: 100px; text-align: right; width: 25%; float: left; line-height: 100px">修复体图片：</div><div style="height: 100px; text-align: center; width: 75%; float: left; line-height: 100px"><img src="images/3.png" width="100px" height="100px"></div>
+        <div style="height: 50px; text-align: right; width: 25%; float: left; line-height: 50px">案例图片：</div><div style="height: 50px; text-align: center; width: 75%; float: left; line-height: 50px"><input type="file" name="file" id="file" value=""></div>
 
       </div>
       <div style="text-align: right; width: 50%; float: left;"><a href="patient.php" data-role="button">提交</a></div><div style="text-align: center; width: 50%; float: left;"><a href="patient.php" data-role="button">提交</a></div>
---
+++ app/shop.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+</head>
+<body>
+
+<div data-role="page">
+  <div data-role="header" data-position="fixed"><a href="#" data-role="button" data-icon="arrow-l" data-rel="back">后退</a>
+  <h1>积分商城</h1>
+  </div>
+  
+
+  <div data-role="content" data-theme="c">
+    <form method="post" action="demoform.asp">
+      
+      
+    </form>
+  </div>
+  
+  <div data-role="footer" data-position="fixed" style="left:0;width:60%;float:left; font-size:16px">
+    <label style="font-size:12px"><h1>可用积分：9999  兑换所需积分：1000</h1></label>
+  </div>
+  <div data-role="footer" data-position="fixed" style="left:60%;width:40%; float:right; background-color:orage">
+    <h1>购买</h1>
+	
+  </div>
+</div>
+
+</body>
+</html>
